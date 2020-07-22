0b1623394cb38db3f2ba617ea9c32a3c96b9b41a
Thomas Wilmot <thomaswilmot@gmail.com>
2020-07-22 13:43:24 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cavalcade",
+    version="1.0.0",
+    author="Thomas Wilmot",
+    author_email="thomaswilmot@gmail.com",
+    description="A lightweight, asynchronous event library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/epkaz93/cavalcade",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
