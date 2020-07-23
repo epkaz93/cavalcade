74c099021b3e55c6ab280a5890949cf6db629041
Thomas Wilmot <thomas.wilmot@al.com.au>
2020-07-23 12:13:51 +1000

    Added event_factory decorator

--- cavalcade/decorators.py
+++ cavalcade/decorators.py
@@ -1,8 +1,15 @@
-from .events import Event
+from .events import Event, EventFactory
 
 
 def event(*args, **kwargs):
     def generate(f):
         e = Event(f, *args, **kwargs)
+        return e
+    return generate
+
+
+def event_factory(*args, **kwargs):
+    def generate(f):
+        e = EventFactory(f, *args, **kwargs)
         return e
     return generate
