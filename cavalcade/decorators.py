baf6819e9916881223b7738d1d6e667267445cd0
Thomas Wilmot <thomaswilmot@gmail.com>
2020-07-23 12:41:27 +1000

    Added requirement.txt

--- cavalcade/decorators.py
+++ cavalcade/decorators.py
@@ -1,8 +1,16 @@
 from .events import Event
+from .graphs import Node
 
 
 def event(*args, **kwargs):
     def generate(f):
         e = Event(f, *args, **kwargs)
+        return e
+    return generate
+
+
+def node(*args, **kwargs):
+    def generate(f):
+        e = Node(f, *args, **kwargs)
         return e
     return generate
